2ef9789705f6d8843c4ab92df9391abc1a443862
Dan Reiland <dmreiland@unixsherpa.com>
2018-04-18 15:42:29 -0500

    Reverted to mongodb v3.2

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
 function log() {
-    local msg=$1
-    echo "===> ${msg}..."
-}
-
-function upgrade_mongod() {
-    mongod --config /etc/mongod.conf --upgrade
+  local msg=$1
+  echo "===> ${msg}..."
 }
 
 function start_servers() {
-    mongod --config /etc/mongod.conf && \
+    mongod --journal --config /etc/mongod.conf & \
     /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Xmx512M -jar /usr/lib/unifi/lib/ace.jar start
 }
 
 # main
-log "upgrading mongod"
-upgrade_mongod
 log "starting mongod, unifi"
 start_servers
