e600d76daaa7e21a7dde2d2bd10b15dfc3ba6a40
Dan Reiland <dmreiland@unixsherpa.com>
2018-04-18 15:40:55 -0500

    Enabled journaling via cli arg

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,8 +6,7 @@ function log() {
 }
 
 function start_servers() {
-    #systemctl start mongod && \
-    mongod --config /etc/mongod.conf && \
+    mongod --journal --config /etc/mongod.conf & \
     /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -Xmx512M -jar /usr/lib/unifi/lib/ace.jar start
 }
 
